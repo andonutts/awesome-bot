170d190da526190b1e8af78f9b886ed2b9e222c6
Andrew He <Andrew He>
2017-04-08 23:03:34 -0500

    Added !magic8ball function

--- mybot.js
+++ mybot.js
@@ -2,12 +2,56 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require('./config.json');
 
-client.login("config.token");
+var magic8Response = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes, definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+];
+
+let responseObject = {
+  "ayy": "Ayy, lmao!",
+  "wat": "Say what?",
+  "lol": "roflmaotntpmp"
+};
+
+client.login(config.token);
+
+client.on("ready", () => {
+  console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
+});
 
 client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix)) return;
+  // Exit if any bot
   if (message.author.bot) return;
 
+  // Message reply array
+  if(responseObject[message.content]) {
+    message.channel.sendMessage(responseObject[message.content]);
+  }
+
+  // Begin prefix-activated replies
+  if (!message.content.startsWith(config.prefix)) return;
+
+  if (message.content.startsWith(config.prefix + "magic8ball")) {
+    message.channel.sendMessage(magic8Response[getRandomInt(0,19)]);
+  } else
   if (message.content.startsWith(config.prefix + "ping")) {
     message.channel.sendMessage("pong!");
   } else
@@ -18,3 +62,8 @@ client.on("message", (message) => {
     message.channel.sendMessage("bar!");
   }
 });
+
+function getRandomInt(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
